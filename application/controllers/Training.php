8a65761f5518609279d09cf21e5c9c07a70b7947
Huỳnh Khoa Vin <khoavin@gmail.com>
2017-05-15 20:22:15 +0700

    -Training

--- application/controllers/Training.php
+++ application/controllers/Training.php
@@ -29,12 +29,12 @@ class Training extends CI_Controller{
             echo json_encode(array('data'=>$result->result_array()));
             $this->db->close();
     }
-    public function getlessonunit(){
+    public function get_lesson_unit(){
         $ls_id = $this->input->post('ls_id');
         $this->load->database();
             $query = "CALL get_lesson_unit(?)";
             $result = $this->db->query($query,array('p_lesson_id' => $ls_id));
-            echo json_encode(array('data'=>$result->result_array()));
+            echo json_encode($result->result_array());
             $this->db->close();
     }
     public function check_lesson_was_bought(){
@@ -43,6 +43,24 @@ class Training extends CI_Controller{
         $this->load->database();
             $query = "CALL check_lesson_was_bought(?,?)";
             $result = $this->db->query($query,array('p_user_id' => $user_id,'p_ls_id' => $lesson_id));
+            echo json_encode($result->result_array());
+            $this->db->close();
+    }
+    public function update_lesson_progress(){
+        $user_id = $this->input->post('user_id');
+        $lu_id = $this->input->post('lu_id');
+        $this->load->database();
+            $query = "CALL update_lesson(?,?)";
+            $result = $this->db->query($query,array('p_user_id' => $user_id,'p_lu_id' => $lu_id));
+            echo json_encode($result->result_array());
+            $this->db->close();
+    }
+    public function get_lesson_tracker(){
+        $user_id = $this->input->post('user_id');
+        $ls_id = $this->input->post('ls_id');
+        $this->load->database();
+            $query = "CALL get_lesson_tracker(?,?)";
+            $result = $this->db->query($query,array('p_user_id' => $user_id,'p_ls_id' => $ls_id));
             echo json_encode($result->result_array());
             $this->db->close();
     }
