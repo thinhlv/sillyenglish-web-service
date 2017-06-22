00acc4b5d124253cc58aa538cdadcaa965d7e361
Huỳnh Khoa Vin <khoavin@gmail.com>
2017-06-22 10:32:59 +0700

    fix friends

--- application/controllers/Friend.php
+++ application/controllers/Friend.php
@@ -5,8 +5,10 @@ class Friend extends CI_Controller {
     }
     public function find_friends() {
         $this->load->database();
-        $query = "CALL find_friends()";
-        $result = $this->db->query($query);
+        $user_id = $this->input->post('user_id');
+        $search_keyword = $this->input->post('search_keyword');
+        $query = "CALL find_friends(?,?)";
+        $result = $this->db->query($query,array('p_user_id'=>$user_id,'p_search_keyword'=>$search_keyword));
         echo json_encode($result->result_array());
         $this->db->close();
     }
